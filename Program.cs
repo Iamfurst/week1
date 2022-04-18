2022e67a5948096d3048d98275679c04d1346b57
Prince Onuoha <princeonuohja3@gmail.com>
2022-04-18 14:58:47 +0100

    week1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿ using System;
+
+namespace week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Prince");
+            Console.WriteLine("I am in Backend track of Wella Schools");
+            Console.WriteLine("Backend development helps me get acquainted with several programming languages and it boost my career.");
+        }
+    }
+}
